3c534dc8f030cef86e851da872503efabe835119
Adrian McCarthy <github@aidtopia.com>
2022-01-14 22:30:18 -0800

    Removed cruft which helps with stability.
    Created the illusion of rotation by animating the position in
    the pattern where scanning starts.

--- code/laser_tunnel/laser_tunnel.ino
+++ code/laser_tunnel/laser_tunnel.ino
@@ -9,68 +9,60 @@ constexpr auto fan_tach_pin = 2;  // Must be a pin that generates external inter
 constexpr auto fan_pwm_pin  = 3;
 constexpr auto laser_pwm_pin = 6;
 
-uint32_t pattern = 0b10101010111100110001000100010011;
-volatile auto active = pattern;
+// Each bit in the pattern determines when the laser should
+// switch on or off.  Scanning the pattern begins with each
+// falling edge in the fan's tachometer signal, which occurs
+// twice per revolution.  Scanning wraps if the entire pattern
+// is used before the next techometer pulse.
+const uint32_t pattern = 0b11111111111100000000000100000000UL;
+// The bit in `scan_start` determines where in the pattern
+// scanning begins.
+volatile uint32_t scan_start = 1UL << 31;
+// The bit in the `scan_pos` indicates the current position in
+// the scan.
+volatile uint32_t scan_pos = scan_start;
 
-volatile long fan_rpm = 0;
-volatile unsigned long prev_fan_pulse_time = 0;
-volatile unsigned long expected_fan_pulse_time = 0;
-
-// Interrupt service routine to update `fan_rpm` and
-// `prev_fan_pulse_time`.
 void fanPulseISR() {
-  constexpr auto tach_pulses_per_rev = 2;
-  const auto now = (micros() + 50)/100;
-  const auto elapsed = now - prev_fan_pulse_time;
-  const auto rpm = 600000/elapsed/tach_pulses_per_rev;
-  fan_rpm = (3*fan_rpm + rpm + 2) / 4;
-  prev_fan_pulse_time = now;
-  expected_fan_pulse_time = now + 2*elapsed;  // estimated
-  // Restart Timer/Counter2 and reset the pattern of laser pulses
+  // Restart Timer/Counter2 and the `scan_pos`.  This will keep
+  // the pattern aligned with the tachometer pulses.
   TCNT2 = 0;
-  active = pattern;
+  scan_pos = scan_start;
+
+  // By animating `scan_start`, the tunnel appears to revolve.
+  scan_start <<= 1;
+  if (scan_start == 0) scan_start = 1;
 }
 
 ISR(TIMER2_COMPA_vect) {
   static const auto port = digitalPinToPort(laser_pwm_pin);
   static const auto mask = digitalPinToBitMask(laser_pwm_pin);
-  static volatile uint8_t *reg = portOutputRegister(port);
-  if (active & 0b1) {
+  static uint8_t volatile * const reg = portOutputRegister(port);
+  if (scan_pos & pattern) {
     *reg |= mask;
   } else {
     *reg &= ~mask;
   }
-  active >>= 1;
-}
-
-long getFanRPM() {
-  noInterrupts();
-  if (micros()/100 > expected_fan_pulse_time) {
-    fan_rpm /= 2;
-  }
-  interrupts();
-  return fan_rpm;
+  scan_pos >>= 1;
+  if (scan_pos == 0) scan_pos = (1UL << 31);
 }
 
 void setup() {
   Serial.begin(115200);
+  Serial.println("Laser Tunnel by Hayward Haunter");
 
   pinMode(laser_pwm_pin, OUTPUT);
   digitalWrite(laser_pwm_pin, LOW);
 
   pinMode(fan_tach_pin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(fan_tach_pin), fanPulseISR, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(fan_tach_pin), fanPulseISR, FALLING);
 
   pinMode(fan_pwm_pin, OUTPUT);
   digitalWrite(fan_pwm_pin, HIGH);
 
-  delay(500);  // Give the fan a chance to spin up..
-
-  // Use Timer/Counter2 to generate interrupts every 400 microseconds.
-  // In the ISR, we'll whether the laser should be on or off.  We'll
-  // restart the timer each time we get a pulse from the tachometer
-  // (twice per revolution), so our pattern of laser dots should be
-  // stable.
+  // Use Timer/Counter2 to generate interrupts.  In the ISR, we'll
+  // whether the laser should be on or off.  We'll restart the timer
+  // each time we get a pulse from the tachometer (twice per
+  // revolution), so our pattern of laser dots should be stable.
   noInterrupts();
   // Set the wave generation mode (3 bits across two registers) to CTC to OCR2A.
   TCCR2A = (TCCR2A & 0b11111100) | (1 << WGM21);
@@ -78,44 +70,11 @@ void setup() {
   // Set the clock source prescaler to 64.
   TCCR2B = (TCCR2B & 0b11111000) | (1 << CS22);
   TCNT2 = 0;  // start counting from 0.
-  OCR2A = 64;  // how high to count
+  OCR2A = 16;  // how high to count
   TIMSK2 |= (1 << OCIE2A);  // enable interrupt each time the counter reaches the limit
   interrupts();
-
-  Serial.println("targetRPM fanRPM fanPWM");
 }
 
 void loop() {
-  static long target_rpm = 1000;
-  static long i_error = 0;
-  static long last_error = 0;
-  const auto actual_rpm = getFanRPM();
-  const auto error = target_rpm - actual_rpm;
-  i_error += error;
-  const auto d_error = error - last_error;
-  last_error = error;
-  int duty_cycle = (error + 5)/10 + (i_error + 10)/20 + (d_error + 15) / 30;
-  if (duty_cycle < 0) duty_cycle = 0;
-  if (255 < duty_cycle) duty_cycle = 255;
-  //analogWrite(fan_pwm_pin, duty_cycle);  
-  Serial.print(target_rpm);
-  Serial.print(' ');
-  Serial.print(actual_rpm);
-  Serial.print(' ');
-  Serial.println(duty_cycle);
-
-  static char buffer[64];
-  static int pos = 0;
-  while (Serial.available() > 0) {
-    char ch = Serial.read();
-    if (ch == '\n' || pos + 1 == sizeof(buffer)) {
-      buffer[pos] = '\0';
-      target_rpm = atoi(buffer);
-      pos = 0;
-    } else if ('0' <= ch && ch <= '9') {
-      buffer[pos++] = ch;
-    }
-  }
-
   delay(250);
 }
