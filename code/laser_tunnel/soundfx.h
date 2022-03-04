50614820cfa031a08f2a8fc605f061c669feaa06
Adrian McCarthy <github@aidtopia.com>
2022-03-04 09:40:46 -0800

    Added a cache of durations of SoundFX tracks.
    This information may be useful to animations and fog timing.

--- code/laser_tunnel/soundfx.h
+++ code/laser_tunnel/soundfx.h
@@ -18,8 +18,9 @@ class SoundFX : public SOUNDFX_BASE_CLASS {
       m_serial(rx_pin, tx_pin),
       m_busy(busy_pin),
       m_module(m_serial),
+      m_file_playing(0),
       m_file_count(0),
-      m_file_playing(0) {}
+      m_durations() {}
 
     void begin() {
       m_busy.begin();
@@ -30,12 +31,27 @@ class SoundFX : public SOUNDFX_BASE_CLASS {
 
     // The Track values are the required file indexes for the sound
     // effects.  User many include any number of sounds, as long as
-    // they are arranged in this order.
-    enum Track : uint16_t { NONE=0, STARTLE=1, AMBIENT=2, EMERGENCY=3 };
+    // the first ones are arranged in this order.
+    enum Track : uint16_t {
+      NONE,
+      STARTLE,
+      AMBIENT,
+      EMERGENCY,
+      SLOTS
+    };
 
     bool has(Track track) const {
       const auto file_index = static_cast<uint16_t>(track);
       return m_file_count >= file_index;
+    }
+
+    // Returns the duration of the requested track in milliseconds,
+    // or 0 if it's not yet known.
+    unsigned long duration(Track track) const {
+      if (track <= NONE || SLOTS <= track) return 0uL;
+      const auto file_index = static_cast<uint16_t>(track);
+      if (file_index > m_file_count) return 0uL;
+      return m_durations[file_index];
     }
 
     Track currentTrack() const {
@@ -51,6 +67,15 @@ class SoundFX : public SOUNDFX_BASE_CLASS {
         return;
       }
       const auto file_index = static_cast<uint16_t>(track);
+
+      // If we don't know the duration of this track, stash
+      // the start time in the NONE slot of m_durations.
+      if (file_index < SLOTS) {
+        if (m_durations[file_index] == 0) {
+          m_durations[NONE] = millis();
+        }
+      }
+      
       m_module.playFile(file_index, Audio::NO_FEEDBACK);
       m_file_playing = file_index;
     }
@@ -60,7 +85,7 @@ class SoundFX : public SOUNDFX_BASE_CLASS {
     void onDeviceInserted(Device src) override {
       SOUNDFX_BASE_CLASS::onDeviceInserted(src);
       if (src == Audio::DEV_SDCARD) {
-        m_file_count = 0;
+        clearCache();
         m_file_playing = 0;
         m_module.queryFileCount(Audio::DEV_SDCARD);
       }
@@ -76,8 +101,8 @@ class SoundFX : public SOUNDFX_BASE_CLASS {
     void onDeviceRemoved(Device src) override {
       SOUNDFX_BASE_CLASS::onDeviceRemoved(src);
       if (src == Audio::DEV_SDCARD) {
-        m_file_count = 0;
         m_file_playing = 0;
+        clearCache();
       }
     }
 
@@ -89,30 +114,39 @@ class SoundFX : public SOUNDFX_BASE_CLASS {
       // to this by sending a source select or you'll get stuck
       // in an endless loop.
       if (devices & (1u << Audio::DEV_SDCARD)) {
+        clearCache();
         m_module.queryFileCount(Audio::DEV_SDCARD);
       }
     }
 
     void onFinishedFile(Device device, uint16_t file_index) override {
       SOUNDFX_BASE_CLASS::onFinishedFile(device, file_index);
-      if (device == Audio::DEV_SDCARD && file_index == m_file_playing) {
-        m_file_playing = 0;
+      if (device != Audio::DEV_SDCARD) return;
+      if (file_index != m_file_playing) return;
+
+      // If we were timing this track, record its duration.
+      if (m_file_playing <= SLOTS && m_durations[m_file_playing] == 0uL) {
+        m_durations[m_file_playing] = millis() - m_durations[NONE];
+        m_durations[NONE] = 0;
       }
-#if 0
-      // The ambient sound should loop.  In theory, the audio module
-      // can do that, but it doesn't seem to work on all models, at
-      // least, not with short clips.  So if the ambient track is
-      // the one that just finished, we'll restart it.
-      if (file_index == AMBIENT) play(AMBIENT);
-#endif
+
+      m_file_playing = 0;
     }
 
   private:
+    void clearCache() {
+      m_file_count = 0;
+      for (auto &d : m_durations) d = 0uL;
+    }
+
     SoftwareSerial m_serial;
     DigitalInputPin m_busy;
     AudioModule<SoftwareSerial> m_module;
-    uint16_t m_file_count;
     uint16_t m_file_playing;
+
+    // Cached metadata about the audio tracks.
+    uint16_t m_file_count;
+    unsigned long m_durations[SLOTS];
 };
 
 #undef SOUNDFX_BASE_CLASS
